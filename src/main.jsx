38d28397609f2ce9ef6c01ebfcbf44c8a991a6ce
Hr1fkELcK61dmQ2aYANM <Hr1fkELcK61dmQ2aYANM@protonmail.com>
2025-05-22 23:02:57 +0800

    changed Todo.js to Todo.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Todo from "./Todo.js";
+import Todo from "./Todo";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
